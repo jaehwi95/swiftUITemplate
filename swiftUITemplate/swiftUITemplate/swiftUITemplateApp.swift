04203645b797e48b0ccce495554162816dc57cae
Jaehwi Kim <jaehwi95@gmail.com>
2024-05-18 19:52:43 +0900

    initial commit

---
+++ swiftUITemplate/swiftUITemplate/swiftUITemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUITemplateApp.swift
+//  swiftUITemplate
+//
+//  Created by Jaehwi Kim on 5/18/24.
+//
+
+import SwiftUI
+
+@main
+struct swiftUITemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
